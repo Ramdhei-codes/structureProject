1d0078ff24c37def345062d6f69623524bef4615
Ramdhei <ramdhei2002@gmail.com>
2022-03-09 10:52:40 -0500

    Marvel API response

--- routes/marvelRouter.js
+++ routes/marvelRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 const privateKey = 'dd3dc844307cf12d76810fcf93ee045373607fa1';
 const publicKey = 'dad66d39785a779dfec6089c9393a02f';
@@ -9,11 +10,12 @@ const ts = Date.now();
 const md5 = require('md5');
 const hash = md5(ts + privateKey + publicKey);
 
-router.get('/', (req, res) => {
-  const URI = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+router.get('/', async (req, res) => {
+  const URI = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
 
-  res.json({ ts, hash, URI});
+  const response = await axios.get(URI);
 
+  res.json(response.data);
 });
 
 module.exports = router;
